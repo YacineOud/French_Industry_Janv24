57dbe4e258936f4be5ddf4104590cd4d16a6c9b7
ChristopheMontoriol <164173108+ChristopheMontoriol@users.noreply.github.com>
2024-08-07 17:52:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,120 @@
 import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-import math
-from pathlib import Path
+import plotly.express as px
+import plotly.graph_objects as go
+import statsmodels.api as sm
+import io
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+# Pour éviter d'avoir les messages warning
+import warnings
+warnings.filterwarnings('ignore')
 
+# Titre de l'application
+st.title('Import des DataSets')
+
+# Charger les données
 @st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+def load_data():
+    etablissement = pd.read_csv('Z:/Data Analyst/Formation_DataAnalyst/Projet/french_industry/base_etablissement_par_tranche_effectif.csv', sep=',')
+    geographic = pd.read_csv('Z:/Data Analyst/Formation_DataAnalyst/Projet/french_industry/name_geographic_information.csv', sep=',')
+    population = pd.read_csv('Z:/Data Analyst/Formation_DataAnalyst/Projet/french_industry/population.csv', sep=',')
+    salaire = pd.read_csv('Z:/Data Analyst/Formation_DataAnalyst/Projet/french_industry/net_salary_per_town_categories.csv', sep=',')
+    return etablissement, geographic, population, salaire
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+etablissement, geographic, population, salaire = load_data()
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+# Pré-processing
+# ---------------------------------------------------------------------------------------------------------------
+# PREPARATION DU DATAFRAME SALAIRE
+# ---------------------------------------------------------------------------------------------------------------
+# Dictionnaire de correspondance entre anciens et nouveaux noms de colonne du dataframe salaire
+new_column_names_salaire = {
+    'SNHM14': 'salaire',
+    'SNHMC14': 'salaire_cadre',
+    'SNHMP14': 'salaire_cadre_moyen',
+    'SNHME14': 'salaire_employe',
+    'SNHMO14': 'salaire_travailleur',
+    'SNHMF14': 'salaire_femme',
+    'SNHMFC14': 'salaire_cadre_femme',
+    'SNHMFP14': 'salaire_cadre_moyen_femme',
+    'SNHMFE14': 'salaire_employe_femme',
+    'SNHMFO14': 'salaire_travailleur_femme',
+    'SNHMH14': 'salaire_homme',
+    'SNHMHC14': 'salaire_cadre_homme',
+    'SNHMHP14': 'salaire_cadre_moyen_homme',
+    'SNHMHE14': 'salaire_employe_homme',
+    'SNHMHO14': 'salaire_travailleur_homme',
+    'SNHM1814': 'salaire_18-25',
+    'SNHM2614': 'salaire_26-50',
+    'SNHM5014': 'salaire_+50',
+    'SNHMF1814': 'salaire_18-25_femme',
+    'SNHMF2614': 'salaire_26-50_femme',
+    'SNHMF5014': 'salaire_+50_femme',
+    'SNHMH1814': 'salaire_18-25_homme',
+    'SNHMH2614': 'salaire_26-50_homme',
+    'SNHMH5014': 'salaire_+50_homme'
+}
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+# Renommer les colonnes du dataframe salaire
+salaire = salaire.rename(columns=new_column_names_salaire)
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+# Supprimer les zéros en première position
+salaire['CODGEO'] = salaire['CODGEO'].str.lstrip('0')
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+# Remplacer les lettres A ou B par des zéros
+salaire['CODGEO'] = salaire['CODGEO'].str.replace('A', '0').str.replace('B', '0')
 
-    return gdp_df
+# Afficher les données chargées
+st.write("Colonnes du dataset 'etablissement' :")
+st.write(etablissement.columns)
+st.write("Colonnes du dataset 'geographic' :")
+st.write(geographic.columns)
+st.write("Colonnes du dataset 'population' :")
+st.write(population.columns)
+st.write("Colonnes du dataset 'salaire' :")
+st.write(salaire.columns)
 
-gdp_df = get_gdp_data()
+# Afficher un aperçu des données
+st.write("Aperçu des données 'etablissement' :")
+st.dataframe(etablissement.head(10))
 
-# -----------------------------------------------------------------------------
-# Draw the actual page
+st.write("Aperçu des données 'geographic' :")
+st.dataframe(geographic.head(10))
 
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
+st.write("Aperçu des données 'population' :")
+st.dataframe(population.head(10))
 
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
+st.write("Aperçu des données 'salaire' :")
+st.dataframe(salaire.head(10))
 
-# Add some spacing
-''
-''
+# Afficher les informations sur les datasets
+st.write("Information sur les données 'etablissement' :")
+buffer = io.StringIO()
+etablissement.info(buf=buffer)
+s = buffer.getvalue()
+st.text(s)
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+st.write("Information sur les données 'geographic' :")
+buffer = io.StringIO()
+geographic.info(buf=buffer)
+s = buffer.getvalue()
+st.text(s)
 
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
+st.write("Information sur les données 'population' :")
+buffer = io.StringIO()
+population.info(buf=buffer)
+s = buffer.getvalue()
+st.text(s)
 
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+st.write("Information sur les données 'salaire' :")
+buffer = io.StringIO()
+salaire.info(buf=buffer)
+s = buffer.getvalue()
+st.text(s)
